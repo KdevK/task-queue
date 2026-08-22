fbc49c8ab942ce59dc10f0a8168416cd2cdfb66f
Konstantin <kostya.kobylin@gmail.com>
2026-08-22 07:30:07 +0300

    test: Publish()

---
+++ internal/broker/memory_test.go
@@ -0,0 +1,94 @@
+package broker
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"task-queue/internal/domain"
+	"testing"
+	"time"
+)
+
+func TestInMemoryBroker_Publish_Success(t *testing.T) {
+	broker := NewInMemoryBroker(5)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	task := domain.Task{
+		ID:          "testID",
+		Type:        "Task",
+		Payload:     nil,
+		Status:      "Pending",
+		RetryCount:  0,
+		MaxRetries:  5,
+		CreatedAt:   time.Now(),
+		ScheduledAt: time.Now(),
+	}
+
+	err := broker.Publish(ctx, &task)
+	if err != nil {
+		t.Errorf("publish failed: %v", err)
+	}
+
+}
+
+func TestInMemoryBroker_Publish_FullChannel(t *testing.T) {
+	const bufferSize = 5
+
+	broker := NewInMemoryBroker(bufferSize)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for i := 0; i < bufferSize; i++ {
+		localTask := domain.Task{
+			ID:          fmt.Sprintf("TaskID%v", i),
+			Type:        "Task",
+			Payload:     nil,
+			Status:      "Pending",
+			RetryCount:  0,
+			MaxRetries:  5,
+			CreatedAt:   time.Now(),
+			ScheduledAt: time.Now(),
+		}
+		broker.tasks <- &localTask
+	}
+
+	task := domain.Task{
+		ID:          "TestID",
+		Type:        "Task",
+		Payload:     nil,
+		Status:      "Pending",
+		RetryCount:  0,
+		MaxRetries:  5,
+		CreatedAt:   time.Now(),
+		ScheduledAt: time.Now(),
+	}
+
+	err := broker.Publish(ctx, &task)
+	if !errors.Is(err, ErrQueueFull) {
+		t.Errorf("expected %v, got: %v", ErrQueueFull, err)
+	}
+}
+
+func TestInMemoryBroker_Publish_CancelledCtx(t *testing.T) {
+	broker := NewInMemoryBroker(5)
+	task := domain.Task{
+		ID:          "TaskID",
+		Type:        "Task",
+		Payload:     nil,
+		Status:      "Pending",
+		RetryCount:  0,
+		MaxRetries:  5,
+		CreatedAt:   time.Now(),
+		ScheduledAt: time.Now(),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// context is cancelled immediately
+	cancel()
+
+	err := broker.Publish(ctx, &task)
+	if !errors.Is(err, ctx.Err()) {
+		t.Errorf("expected %v, got: %v", ctx.Err(), err)
+	}
+}
