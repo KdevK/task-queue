87abfb0830672cabea7e0b903bf766e8e9e1ef5b
vkdevk <kostya.kobylin@gmail.com>
2026-08-27 23:00:42 +0300

    add: postgres repository implementation

--- internal/storage/postgres/repository.go
+++ internal/storage/postgres/repository.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"task-queue/internal/domain"
 	"task-queue/internal/storage"
@@ -43,18 +44,39 @@ func (r *Repository) Create(ctx context.Context, task *domain.Task) error {
 
 func (r *Repository) GetByID(ctx context.Context, id string) (*domain.Task, error) {
 	query := `
-		SELECT * FROM tasks WHERE id = $1
+		SELECT id, type, payload, status, retry_count, max_retries, created_at, scheduled_at
+		FROM tasks WHERE id = $1
 	`
-	rows, err := r.pool.Query(ctx, query, id)
+	var task domain.Task
+	err := r.pool.QueryRow(ctx, query, id).Scan(
+		&task.ID,
+		&task.Type,
+		&task.Payload,
+		&task.Status,
+		&task.RetryCount,
+		&task.MaxRetries,
+		&task.CreatedAt,
+		&task.ScheduledAt,
+	)
 	if err != nil {
-		return nil, storage.ErrNotFound
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, storage.ErrNotFound
+		}
+		return nil, fmt.Errorf("storage: failed to get task: %w", err)
 	}
-	defer rows.Close()
+	return &task, nil
+}
 
-	user, userErr := pgx.CollectOneRow(rows, pgx.RowToStructByName[domain.Task])
-	if userErr != nil {
-		return nil, fmt.Errorf("storage: failed to map data to task: %w", err)
+func (r *Repository) UpdateStatus(ctx context.Context, id string, status domain.Status) error {
+	query := `
+		UPDATE tasks SET status = $1 WHERE id = $2
+	`
+	tag, err := r.pool.Exec(ctx, query, status, id)
+	if err != nil {
+		return fmt.Errorf("storage: failed to update task: %w", err)
 	}
-
-	return &user, nil
+	if tag.RowsAffected() == 0 {
+		return storage.ErrNotFound
+	}
+	return nil
 }
