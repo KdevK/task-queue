09638dd484c8f2602c9e79d48351903916f3ff72
Konstantin <kostya.kobylin@gmail.com>
2026-09-04 23:16:53 +0300

    add: registry for handlers depending on task type

---
+++ internal/worker/registry.go
@@ -0,0 +1,32 @@
+package worker
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type Handler func(ctx context.Context, payload json.RawMessage) error
+
+type Registry struct {
+	handlers map[string]Handler
+}
+
+func NewRegistry() *Registry {
+	return &Registry{make(map[string]Handler)}
+}
+
+func (r *Registry) Register(taskType string, h Handler) {
+	_, ok := r.handlers[taskType]
+	if ok {
+		panic("handler for this task type already exists")
+	}
+	r.handlers[taskType] = h
+}
+
+func (r *Registry) Get(taskType string) (Handler, bool) {
+	h, ok := r.handlers[taskType]
+	if !ok {
+		return nil, false
+	}
+	return h, true
+}
